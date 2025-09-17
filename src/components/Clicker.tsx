701ade527cddd8176aa34c8ed3ca905f0cd76de0
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-09-17 23:19:35 +0100

    feat: dynamically add power orbs based on clicker count

--- src/components/Clicker.tsx
+++ src/components/Clicker.tsx
@@ -1,12 +1,23 @@
 import { useState } from "react";
 import Orb from "./Orb";
+import PowerOrb from "./PowerOrb";
 
 const Clicker = () => {
   const [count, setCount] = useState(0);
 
+  const POWER_ORB_THRESHOLD = 100;
+  const POWER_ORB_MAX_COUNT = 4;
+
+  const powerOrbs = Math.floor(count / POWER_ORB_THRESHOLD);
+  const result = Math.min(powerOrbs, POWER_ORB_MAX_COUNT);
+  const orbs = [...Array(result)];
+
   return (
     <div>
       <Orb count={count} setCount={setCount} />
+      {orbs.map((_, index) => (
+        <PowerOrb key={index} count={count} setCount={setCount} increment={(index + 1) * 5 } />
+      ))}
     </div>
   );
 };
