ab790962ddbe757837630ce54ec8290cfbde3c12
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-09-17 19:46:26 +0100

    style: add initial styling

--- src/components/Orb.tsx
+++ src/components/Orb.tsx
@@ -1,4 +1,28 @@
 import { useState } from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  aspect-ratio: 1 / 1;
+  border-radius: 50rem;
+  cursor: pointer;
+  height: 30dvh;
+  width: auto;
+  background: inherit;
+  border: 4px solid gray;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:active {
+    box-shadow: none;
+    background: inherit;
+  }
+`
 
 const Orb = () => {
   const [count, setCount] = useState(0);
@@ -9,7 +33,7 @@ const Orb = () => {
 
   return (
     <div>
-      <button onClick={handleCount}>{count}</button>
+      <Button onClick={handleCount}>{count}</Button>
     </div>
   );
 };
