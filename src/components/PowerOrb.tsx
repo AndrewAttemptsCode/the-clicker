c05bc727a2b50315fc38a04e8551cb95959b4fb1
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-09-18 15:37:38 +0100

    feat: add animated button background based on button disabled duration

--- src/components/PowerOrb.tsx
+++ src/components/PowerOrb.tsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
-import styled from "styled-components";
+import styled, { css, keyframes } from "styled-components";
+
+const slideUp = keyframes`
+  0% {
+    transform: translateY(100%);
+  }
+  100% {
+    transform: translateY(0%);
+  }
+`;
 
 const Button = styled.button`
+  position: relative;
   aspect-ratio: 1 / 1;
   border-radius: 50rem;
   cursor: pointer;
   height: 4rem;
   width: auto;
   background: inherit;
-  border: 4px solid gray;
+  border: 2px solid gray;
+  overflow: hidden;
 
   display: flex;
   align-items: center;
@@ -22,15 +33,33 @@ const Button = styled.button`
     box-shadow: none;
     background: inherit;
   }
-`
+`;
+
+const OrbBackground = styled.div<OrbBackgroundProps>`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  background-color: aqua;
+  ${({ $orbClicked, $duration }) =>
+    $orbClicked &&
+    css`
+      animation: ${slideUp} ${$duration}ms ease-in-out forwards;
+    `}
+`;
+
+type OrbBackgroundProps = {
+  $orbClicked: boolean;
+  $duration: number;
+};
 
 type PowerOrbProps = {
   count: number;
   setCount: (value: number) => void;
   increment: number;
-}
+};
 
-const PowerOrb = ({count, setCount, increment }: PowerOrbProps) => {
+const PowerOrb = ({ count, setCount, increment }: PowerOrbProps) => {
   const [disableOrb, setDisableOrb] = useState(false);
 
   const POWER_ORB_TIMEOUT = increment * 2000;
@@ -42,9 +71,14 @@ const PowerOrb = ({count, setCount, increment }: PowerOrbProps) => {
     setTimeout(() => {
       setDisableOrb(false);
     }, POWER_ORB_TIMEOUT);
-  }
+  };
 
-  return <Button disabled={disableOrb} onClick={handleClick}>+ {increment}</Button>;
+  return (
+    <Button disabled={disableOrb} onClick={handleClick}>
+      + {increment}
+      <OrbBackground $orbClicked={disableOrb} $duration={POWER_ORB_TIMEOUT} />
+    </Button>
+  );
 };
 
 export default PowerOrb;
