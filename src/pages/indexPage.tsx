9edc0737689eef618fad59cee2559a20297f4b3e
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-09-20 15:59:51 +0100

    style: add background color and page layout control

--- src/pages/indexPage.tsx
+++ src/pages/indexPage.tsx
@@ -1,11 +1,18 @@
+import styled from "styled-components";
 import Clicker from "../components/Clicker";
+
+const Container = styled.div`
+  background-color: #22223f;
+  min-height: 100vh;
+  min-height: 100dvh;
+`
 
 const IndexPage = () => {
   return (
-    <div>
+    <Container>
       <p>Index Page</p>
       <Clicker />
-    </div>
+    </Container>
   );
 };
 
