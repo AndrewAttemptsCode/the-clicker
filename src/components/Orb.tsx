0bb58b057c9c41954afa7cb65ecbef7fd890048e
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-09-20 15:17:46 +0100

    feat: add orb background with visual animation tracking based on click count

--- src/components/Orb.tsx
+++ src/components/Orb.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Button = styled.button`
   aspect-ratio: 1 / 1;
@@ -7,20 +7,34 @@ const Button = styled.button`
   height: 30dvh;
   width: 30dvh;
   background: inherit;
-  border: 4px solid gray;
+  border: none;
+  overflow: hidden;
+  box-shadow: 0 0 8px 3px rgb(0, 190, 190);
+  color: rgb(220, 253, 253);
 
   display: flex;
   align-items: center;
   justify-content: center;
+  position: relative;
 
   &:focus {
     outline: none;
   }
 
   &:active {
-    box-shadow: none;
+    box-shadow: 0 0 8px 3px rgb(0, 190, 190);
     background: inherit;
   }
+`
+
+const OrbBackground = styled.div<OrbBackgroundProps>`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  background: linear-gradient(to bottom, rgb(92, 238, 238) , rgb(26, 66, 66));
+  transform-origin: bottom;
+  transform: ${({$scaleTracker}) => $scaleTracker ? `scaleY(${$scaleTracker})` : "none"}; 
 `
 
 type OrbProps = {
@@ -28,14 +42,24 @@ type OrbProps = {
   setCount: (value: number) => void;
 }
 
+type OrbBackgroundProps = {
+  $scaleTracker: number;
+}
+
 const Orb = ({ count, setCount }: OrbProps) => {
+
+  const SCALE = (count % 100) / 100;
   
   const handleIncrement = () => {
     setCount(count + 1);
   }
 
-  return <Button onClick={handleIncrement}>{count}</Button>;
-
+  return (
+    <Button onClick={handleIncrement}>
+      {count}
+      <OrbBackground $scaleTracker={SCALE} />
+    </Button>
+  );
 };
 
 export default Orb;
