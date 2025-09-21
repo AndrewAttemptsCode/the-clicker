cd3e80647280140d567807c37d6ac18e7b8eadcc
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-09-21 23:05:57 +0100

    style: add header layout and styling

--- src/pages/indexPage.tsx
+++ src/pages/indexPage.tsx
@@ -5,12 +5,25 @@ const Container = styled.div`
   background-color: #22223f;
   min-height: 100vh;
   min-height: 100dvh;
+  text-align: center;
+  padding: 2rem;
+`
+
+const PageTitle = styled.h1`
+  font-family: 'Knewave', sans-serif;
+  font-size: 3rem;
+  text-transform: uppercase;
+  color: rgb(220, 253, 253);
+  text-shadow: 2px 2px 5px rgba(0,0,0,0.5);
+  margin-bottom: 2rem;
+  line-height: 1.1;
+  letter-spacing: 0.5rem;
 `
 
 const IndexPage = () => {
   return (
     <Container>
-      <p>Index Page</p>
+      <PageTitle>The Clicker</PageTitle>
       <Clicker />
     </Container>
   );
